543e20f32936afa9b702282c7e1ca1278b10c0f6
Nilesh M P <nileshmp@gmail.com>
2024-11-19 13:39:15 +0530

    Finally most of the work needed to host jenkins done

--- docker-images/jenkins/build-jenkins-4-terraform.sh
+++ docker-images/jenkins/build-jenkins-4-terraform.sh
@@ -1,2 +1,2 @@
 # docker build --no-cache --tag="jenkins-aws-ready" . &> build.log
-docker build --tag="jenkins-aws-ready" .
+docker build --build-arg AWS_ACCESS_KEY=$1 --build-arg AWS_SECRET_KEY=$2 --build-arg AWS_REGION=ap-south-1 --tag="jenkins-aws-ready" .
